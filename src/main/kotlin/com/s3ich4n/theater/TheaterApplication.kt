d897523f6368ee4b0964d3fcbcb6939b97ea5750
Seongeun Yu <s3ich4n@gmail.com>
2023-03-14 00:33:36 +0900

    feat: 초기 프로젝트 설정 완료

---
+++ src/main/kotlin/com/s3ich4n/theater/TheaterApplication.kt
@@ -0,0 +1,11 @@
+package com.s3ich4n.theater
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheaterApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheaterApplication>(*args)
+}
---
+++ src/test/kotlin/com/s3ich4n/theater/TheaterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.s3ich4n.theater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheaterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
